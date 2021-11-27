6ef6d9a17bd9560fa51b5c2463cfd4bc89a681c4
Jagadish Bapanapally <jagadishb285@gmail.com>
2021-11-26 21:37:03 -0700

    Update banach-tarski-1.lisp

--- Banach-tarski/code/banach-tarski-1.lisp
+++ Banach-tarski/code/banach-tarski-1.lisp
@@ -24,6 +24,156 @@
 
 ;; Abstract: The Banach-Tarski theorem states that a solid ball can be partitioned into a finite number of pieces which can be rotated to form two identical copies of the ball. The proof of the Banach-Tarski theorem involves generating a free group of rotations and then decomposing the ball using these rotations and rearranging them to get two copies of the ball. The key ingredients to the proof are the Hausdorff paradox and the vitali's theorem that uses the Axiom of choice. We have proved the Hausdorff paradox and the vitali's theorem in ACL2(r) and now we are working to prove the Banach-Tarski theorem for a solid ball centered at the origin with radius equal to 1.
 
+(encapsulate
+  ()
+  (local (include-book "arithmetic-5/top" :dir :system))
+
+  (defthmd d-p=>d-p-p-1
+    (implies (and (s2-def-p p1)
+                  (point-in-r3 p2)
+                  (equal (aref2 :fake-name p2 0 0) (- (aref2 :fake-name p1 0 0)))
+                  (equal (aref2 :fake-name p2 1 0) (- (aref2 :fake-name p1 1 0)))
+                  (equal (aref2 :fake-name p2 2 0) (- (aref2 :fake-name p1 2 0))))
+             (s2-def-p p2))
+    :hints (("Goal"
+             :in-theory (disable aref2)
+             )))
+  )
+
+(encapsulate
+  ()
+
+  (local (include-book "arithmetic-5/top" :dir :system))
+
+  (defthmd d-p=>d-p-p-2-1
+    (implies (r3-matrixp m)
+             (and (equal (aref2 :fake-name (second (f-poles m)) 0 0)
+                         (- (aref2 :fake-name (first (f-poles m)) 0 0)))
+                  (equal (aref2 :fake-name (second (f-poles m)) 1 0)
+                         (- (aref2 :fake-name (first (f-poles m)) 1 0)))
+                  (equal (aref2 :fake-name (second (f-poles m)) 2 0)
+                         (- (aref2 :fake-name (first (f-poles m)) 2 0)))))
+    :hints (("Goal"
+             :use ((:instance f-poles-prop-2 (m m)))
+             :in-theory (disable aref2 f-poles acl2-sqrt square)
+             )))
+
+  (defthmd d-p=>d-p-p-2-2
+    (implies (and (point-in-r3 p1)
+                  (point-in-r3 p2)
+                  (equal (aref2 :fake-name p2 0 0) (aref2 :fake-name p1 0 0))
+                  (equal (aref2 :fake-name p2 1 0) (aref2 :fake-name p1 1 0))
+                  (equal (aref2 :fake-name p2 2 0) (aref2 :fake-name p1 2 0)))
+             (m-= p1 p2))
+    :hints (("Goal"
+             :in-theory (enable m-=)
+             )))
+  )
+
+----
+
+(encapsulate
+  ()
+
+  (local (include-book "arithmetic-5/top" :dir :system))
+
+  (defthmd d-p=>d-p-p-2
+    (implies (and (d-p p1)
+                  (point-in-r3 p2)
+                  (equal (aref2 :fake-name p2 0 0) (- (aref2 :fake-name p1 0 0)))
+                  (equal (aref2 :fake-name p2 1 0) (- (aref2 :fake-name p1 1 0)))
+                  (equal (aref2 :fake-name p2 2 0) (- (aref2 :fake-name p1 2 0)))
+                  (m-= (first (poles (word-exists-witness p1))) p1))
+             (m-= p2 (second (poles (word-exists-witness p1)))))
+    :hints (("Goal"
+             :use ((:instance s2-def-p (point p1))
+                   (:instance point-in-r3 (x p1))
+                   (:instance d-p=>d-p-p-2-2 (p1 p2) (p2 (second (poles (word-exists-witness p1)))))
+                   (:instance d-p (point p1))
+                   (:instance poles (w (word-exists-witness p1)))
+                   (:instance word-exists (point p1))
+                   (:instance d-p=>d-p-p-2-1 (m (rotation (word-exists-witness p1) (acl2-sqrt 2))))
+                   (:instance r3-rotationp (m (rotation (word-exists-witness p1) (acl2-sqrt 2))))
+                   (:instance f-poles-prop-3 (m (rotation (word-exists-witness p1) (acl2-sqrt 2))))
+                   (:instance rotation-is-r3-rotationp (w (word-exists-witness p1)) (x (acl2-sqrt 2))))
+             :in-theory (e/d (m-=) (aref2 reducedwordp rotation acl2-sqrt square aref2 m-trans r3-m-inverse r3-m-determinant r3-matrixp f-poles word-exists d-p d-p-implies))
+             ))
+    )
+
+  (defthmd d-p=>d-p-p-3
+    (implies (and (d-p p1)
+                  (point-in-r3 p2)
+                  (equal (aref2 :fake-name p2 0 0) (- (aref2 :fake-name p1 0 0)))
+                  (equal (aref2 :fake-name p2 1 0) (- (aref2 :fake-name p1 1 0)))
+                  (equal (aref2 :fake-name p2 2 0) (- (aref2 :fake-name p1 2 0)))
+                  (m-= (second (poles (word-exists-witness p1))) p1))
+             (m-= p2 (first (poles (word-exists-witness p1)))))
+    :hints (("Goal"
+             :use ((:instance s2-def-p (point p1))
+                   (:instance point-in-r3 (x p1))
+                   (:instance d-p=>d-p-p-2-2 (p1 p2) (p2 (first (poles (word-exists-witness p1)))))
+                   (:instance d-p (point p1))
+                   (:instance poles (w (word-exists-witness p1)))
+                   (:instance word-exists (point p1))
+                   (:instance d-p=>d-p-p-2-1 (m (rotation (word-exists-witness p1) (acl2-sqrt 2))))
+                   (:instance r3-rotationp (m (rotation (word-exists-witness p1) (acl2-sqrt 2))))
+                   (:instance f-poles-prop-3 (m (rotation (word-exists-witness p1) (acl2-sqrt 2))))
+                   (:instance rotation-is-r3-rotationp (w (word-exists-witness p1)) (x (acl2-sqrt 2))))
+             :in-theory (e/d (m-=) (aref2 reducedwordp rotation acl2-sqrt square aref2 m-trans r3-m-inverse r3-m-determinant r3-matrixp f-poles word-exists d-p d-p-implies))
+             ))
+    )
+  )
+
+(defthmd d-p=>d-p-p-4
+  (implies (and (m-= p2 pole)
+                (m-= (m-* rot pole) pole))
+           (m-= (m-* rot p2) p2)))
+
+(defthmd d-p=>d-p-p
+  (implies (and (d-p p1)
+                (point-in-r3 p2)
+                (equal (aref2 :fake-name p2 0 0) (- (aref2 :fake-name p1 0 0)))
+                (equal (aref2 :fake-name p2 1 0) (- (aref2 :fake-name p1 1 0)))
+                (equal (aref2 :fake-name p2 2 0) (- (aref2 :fake-name p1 2 0))))
+           (d-p p2))
+  :hints (("Goal"
+           :use ((:instance two-poles-for-all-rotations (p p1)))
+           :cases ((m-= (first (poles (word-exists-witness p1))) p1)
+                   (m-= (second (poles (word-exists-witness p1))) p1))
+           :in-theory (disable reducedwordp d-p word-exists square s2-def-p wordp array2p word-exists-suff)
+           )
+          ("Subgoal 2"
+           :use ((:instance d-p=>d-p-p-2 (p1 p1) (p2 p2))
+                 (:instance d-p (point p1))
+                 (:instance d-p (point p2))
+                 (:instance word-exists-suff (point p2) (w (word-exists-witness p1)))
+                 (:instance word-exists (point p1))
+                 (:instance d-p=>d-p-p-1 (p1 p1) (p2 p2))
+                 (:instance f-returns-poles-1-second (w  (word-exists-witness p1)))
+                 (:instance d-p=>d-p-p-4 (p2 p2) (pole (second (poles (word-exists-witness p1))))
+                            (rot (rotation (word-exists-witness p1) (acl2-sqrt 2))))
+                 )
+           :in-theory nil
+           )
+          ("Subgoal 1"
+           :use ((:instance d-p=>d-p-p-3 (p1 p1) (p2 p2))
+                 (:instance d-p (point p1))
+                 (:instance d-p (point p2))
+                 (:instance word-exists-suff (point p2) (w (word-exists-witness p1)))
+                 (:instance word-exists (point p1))
+                 (:instance d-p=>d-p-p-1 (p1 p1) (p2 p2))
+                 (:instance f-returns-poles-1-first (w  (word-exists-witness p1)))
+                 (:instance d-p=>d-p-p-4 (p2 p2) (pole (first (poles (word-exists-witness p1))))
+                            (rot (rotation (word-exists-witness p1) (acl2-sqrt 2))))
+                 )
+           :in-theory nil
+           )
+
+          )
+  )
+
+----
+
 (defthmd witness-not-in-x-coord-sequence-1
   (and (realp (exists-in-interval-but-not-in-x-coord-sequence-witness -1 1))
        (realp (acl2-sqrt (- 1 (* (exists-in-interval-but-not-in-x-coord-sequence-witness -1 1)
@@ -90,6 +240,37 @@
              :in-theory (enable mod floor1)
              )))
   )
+
+---
+
+(defun-sk exists-int-k (theta1 theta2)
+  (exists k
+          (and (integerp k)
+               (equal (+ theta2 (* 2 k (acl2-pi)))
+                      theta1))))
+
+
+(defthmd test-202
+  (implies (and (point-in-r3 p)
+                (not (m-= (point-on-s2-not-d) p))
+                (realp theta1)
+                (realp theta2)
+                (m-= (m-* (rotation-about-witness theta1 (point-on-s2-not-d)) p)
+                     (m-* (rotation-about-witness theta2 (point-on-s2-not-d)) p)))
+           (exists-int-k theta1 theta2)))
+
+(defthmd test-202
+  (implies (and (not (equal th1 th2))
+                (>= th1 0)
+                (< th1 (* 2 (acl2-pi)))
+                (>= th2 0)
+                (< th2 (* 2 (acl2-pi))))
+           (not (equal (acl2-sine th1) (acl2-sine th2))))
+  :hints (("Goal"
+           :use ((:instance acl2-sine (x th1))
+                 (:instance acl2-sine (x th2)))
+           :in-theory (enable (acl2-sine))
+           )))
 
 (defun-sk exists-natp-x (theta1 k)
   (exists x
